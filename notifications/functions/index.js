87486ff32fc5ff7d65e3eb275fead0478596d008
mr.bhuvaneswaran <mr.bhuvaneswaran@gmail.com>
2017-08-28 21:33:25 +0530

    notification are complete

--- notifications/functions/index.js
+++ notifications/functions/index.js
@@ -7,30 +7,42 @@ exports.sendNotification = functions.database.ref('/Notifications/{user_id}/{not
     const notification_id = event.params.notification_id;
     console.log("The User_id is : ",user_id);
 
-    if(!event.data.value()){
+    if(!event.data.val()){
         console.log('A Notification is deleted : ', notification_id);
         return
     }
 
-    const device_token = admin.database().ref(`/Users/${user_id}/device_token`).once('value');
-
-    return device_token.then(result => {
-
-        const token_id = result.val();
-
-            const payload = {
-            Notification:{
+    const from_user = admin.database().ref(`/Notifications/${user_id}/${notification_id}`).once('value');
+        
+    return from_user.then(userResult => {
+        const from_user_id = userResult.val().from;
+        console.log("You have New Notification from : ", from_user_id);
+    
+        const userQuery = admin.database().ref(`/Users/${from_user_id}/name`).once('value');
+        const device_token = admin.database().ref(`/Users/${user_id}/device_token`).once('value');
+        return Promise.all([userQuery, device_token]).then(result => {
+            console.log("from_user_id",from_user_id);
+            const to_user_name = result[0].val(); 
+            const token_id = result[1].val();
+                   const payload = {
+            notification:{
             title: "FRIEND REQUEST",
-            body: "You have New Friend Request",
-            icon: "default"
-        }
-    };
+            body: `${to_user_name} sent a Friend Request`,
+            icon: "default",
+            click_action : "chatapp.com.chatapp_TARGET_NOTIFICATION",
+            },
+            data : {
+                from_user : from_user_id
+            }
+        };
+          return admin.messaging().sendToDevice(token_id,payload).then(Response =>{
+                console.log("Notification Feature Added")
+            });
+    
+    });
+    
 
-    return admin.messaging().sendToDevice(token_id,payload).then(Response =>{
-        console.log("Notification Feature Added")
-       });
     });
 
-
-    });
+});
 
