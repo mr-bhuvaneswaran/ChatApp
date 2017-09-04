945c270930727c1746ecfe5db3037c3e7d39845a
mr.bhuvaneswaran <mr.bhuvaneswaran@gmail.com>
2017-09-04 07:35:06 +0530

    NAME

--- app/src/main/java/chatapp/com/chatapp/MainActivity.java
+++ app/src/main/java/chatapp/com/chatapp/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("ChatApp");
+        getSupportActionBar().setTitle("FAB");
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
